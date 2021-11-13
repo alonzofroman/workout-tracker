c8819c68969ddc75a20f44704bb90414eaa83c81
alonzofroman <alonzofroman@gmail.com>
2021-11-12 17:52:35 -0800

    Update workout route added

--- routes/api.js
+++ routes/api.js
@@ -10,10 +10,18 @@ router.get('api/workouts', (req, res) => {
 });
 
 router.post('api/workouts/', (req, res) => {
-    Workout.create(body).then(data => {
+    Workout.create(req.body).then(data => {
         res.json(data);
     }).catch(err => {
         res.status(400).json(err);
+    })
+});
+
+router.put('/api/workouts/:id', (req, res) => {
+    Workout.findByIdAndUpdate({_id: req.params.id}, {exercise: req.body}).then((data) => {
+        res.json(data);
+    }).catch(err => {
+        res.json(err);
     })
 });
 
