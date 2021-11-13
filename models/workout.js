70d97d25800aeaa4182bf9e57bc6b1c4d89bac9e
alonzofroman <alonzofroman@gmail.com>
2021-11-12 16:12:46 -0800

    Workout model added

---
+++ models/workout.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        name: {
+            type: String,
+            trim: true,
+            required: "Exercise name required"
+        },
+        type: {
+            type: String,
+            trim: true,
+            required: 'Exercise type required'
+        },
+        weight: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        duration: {
+            type: Number,
+            required: "Exercise duration required"
+        },
+        distance: {
+            type: Number
+        }
+    }]
+});
+const Workout = mongoose.model('workout', workoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -11,10 +11,8 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/tracker', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
     useFindAndModify: false,
 });
 
